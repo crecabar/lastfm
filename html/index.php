199c41be10d19f7ed476e6ce7d592e751f363972
Cristian Recabarren <cristian.recabarren@synacore.cl>
2015-06-10 14:45:09 -0300

    Changed index.php to match syntax requirement from PHP5.4.40

--- html/index.php
+++ html/index.php
@@ -9,7 +9,9 @@ $content = "";
 $content .= \Includes\Helpers::render('templates/header');
 $content .= \Includes\Helpers::render('templates/search-form');
 
-if (!empty($keyword = filter_input(INPUT_GET, 'keyword'))) {
+$keyword = filter_input(INPUT_GET, 'keyword');  // PHP 5.4.40 claims against to have this sentence inside the if
+                                                // sentence, but in PHP 5.5.20 is allowed.
+if (!empty($keyword)) {
     $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT) ? : 0; //TODO: add pager management.
     $lastFm = new Lastfm\LastfmSearch(['page_number' => $page]);
     $content .= \Includes\Helpers::render('templates/list', ['artists' => $lastFm->run($keyword)]);
